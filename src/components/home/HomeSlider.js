67167922623cf05c5500d6e631a15d6463078cbc
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-03-26 10:57:40 +0200

    creating components for Home Page

---
+++ src/components/home/HomeSlider.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class HomeSlider extends Component {
+  render() {
+    return <div>HomeSlider</div>;
+  }
+}
+
+export default HomeSlider;
---
+++ src/components/home/Populars.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Populars extends Component {
+  render() {
+    return <div>Populars</div>;
+  }
+}
+
+export default Populars;
