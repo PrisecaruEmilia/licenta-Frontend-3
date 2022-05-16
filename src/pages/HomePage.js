8441c72b2dd5e7adb957840ca4f9cc2c0cfc1bd4
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-16 09:47:24 +0300

    consume visitor details API client-side

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,10 +6,25 @@ import NewArrival from '../components/home/NewArrival';
 import HomeTop from '../components/home/HomeTop';
 import Populars from '../components/home/Populars';
 import Footer from '../components/common/Footer';
+import AppUrl from '../api/AppUrl';
+import axios from 'axios';
 export class HomePage extends Component {
   componentDidMount() {
     window.scroll(0, 0);
+    this.getVisitorDetails();
   }
+  getVisitorDetails = () => {
+    axios
+      .get(AppUrl.VisitorDetails)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
   render() {
     return (
       <Fragment>
