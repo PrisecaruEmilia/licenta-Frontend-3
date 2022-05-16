7be2a69b0a312898f3489d8f31789b0827c7f321
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-16 13:32:55 +0300

    consume contact Rest API - from client side part 1

--- src/components/common/Contact.js
+++ src/components/common/Contact.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 export class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      message: '',
+    };
+  }
+
+  nameOnChange = (event) => {
+    let name = event.target.value;
+    // alert(name);
+    this.setState({ name: name });
+  };
+
+  emailOnChange = (event) => {
+    let email = event.target.value;
+    // alert(email);
+    this.setState({ email: email });
+  };
+
+  subjectOnChange = (event) => {
+    let subject = event.target.value;
+    // alert(subject);
+    this.setState({ subject: subject });
+  };
+
+  messageOnChange = (event) => {
+    let message = event.target.value;
+    // alert(message);
+    this.setState({ message: message });
+  };
+
+  onFormSubmit = (event) => {
+    alert('Hello hi');
+    event.prventDefault();
+  };
   render() {
     return (
       <section className="contact-page-section">
@@ -9,29 +47,29 @@ export class Contact extends Component {
             <Col className="contact-page-column">
               <section className="mb-4">
                 <h2 className="h1-responsive font-weight-bold text-center my-4">
-                  Contact us
+                  Contact
                 </h2>
                 <p className="text-center w-responsive mx-auto mb-5">
-                  Do you have any questions? Please do not hesitate to contact
-                  us directly. Our team will come back to you within a matter of
-                  hours to help you.
+                  Aveți vre-o intrebare? Vă rugăm nu ezitați să ne contactați
+                  direct. Echipa noastră va reveni la dvs. în câteva ore pentru
+                  a vă ajuta.
                 </p>
 
                 <div className="row">
                   <div className="col-md-9 mb-md-0 mb-5">
                     <form
+                      onSubmit={this.onFormSubmit}
                       id="contact-page-contact-form"
                       name="contact-page-contact-form"
-                      action=""
-                      method=""
                     >
                       <div className="row">
                         <div className="col-md-6">
                           <div className="md-form mb-2">
                             <label htmlFor="contact-page-name-input-field">
-                              Your name
+                              Nume
                             </label>
                             <input
+                              onChange={this.nameOnChange}
                               type="text"
                               id="contact-page-name-input-field"
                               name="contact-page-name-input-field"
@@ -43,10 +81,11 @@ export class Contact extends Component {
                         <div className="col-md-6">
                           <div className="md-form mb-2">
                             <label htmlFor="contact-page-email-input-field">
-                              Your email
+                              Email
                             </label>
                             <input
-                              type="text"
+                              onChange={this.emailOnChange}
+                              type="email"
                               id="contact-page-email-input-field"
                               name="contact-page-email-input-field"
                               className="form-control contact-page-input-field"
@@ -59,9 +98,10 @@ export class Contact extends Component {
                         <div className="col-md-12">
                           <div className="md-form mb-2">
                             <label htmlFor="contact-page-subject-input-field">
-                              Subject
+                              Subiect
                             </label>
                             <input
+                              onChange={this.subjectOnChange}
                               type="text"
                               id="contact-page-subject-input-field"
                               name="contact-page-subject-input-field"
@@ -75,9 +115,10 @@ export class Contact extends Component {
                         <div className="col-md-12">
                           <div className="md-form mb-2">
                             <label htmlFor="contact-page-message-input-textarea">
-                              Your message
+                              Mesaj
                             </label>
                             <textarea
+                              onChange={this.messageOnChange}
                               type="text"
                               id="contact-page-message-input-textarea"
                               name="contact-page-message-input-textarea"
@@ -90,7 +131,10 @@ export class Contact extends Component {
                     </form>
 
                     <div className="text-center text-md-left">
-                      <a className="btn btn-primary contact-page-submit-button">
+                      <a
+                        type="submit"
+                        className="btn btn-primary contact-page-submit-button"
+                      >
                         Send
                       </a>
                     </div>
