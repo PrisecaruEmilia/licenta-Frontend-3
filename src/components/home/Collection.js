b53e6e586857947c80438f798ee6e9b6e95359c3
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-17 17:54:59 +0300

    consume product list API - part 2 collection

--- src/components/home/Collection.js
+++ src/components/home/Collection.js
@@ -2,9 +2,119 @@ import React, { Component, Fragment } from 'react';
 import { Col, Container, Row, Card } from 'react-bootstrap';
 import shoe_4 from '../../assets/images/shoe_4.png';
 import shoe_3 from '../../assets/images/shoe_3.png';
-
+import { Link } from 'react-router-dom';
+import AppURL from '../../api/AppUrl';
+import axios from 'axios';
 export class Collection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productData: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.ProductListByRemark('collection'))
+      .then((response) => {
+        this.setState({ productData: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
+    let updatedData;
+    if (this.state.productData.length >= 2) {
+      updatedData = [...this.state.productData].splice(0, 2);
+    } else {
+      updatedData = [...this.state.productData];
+    }
+
+    const CollectionList = updatedData;
+    let classConstraint;
+    let subcategoryUpper;
+    const RenderView = CollectionList.map((CollectionList, i) => {
+      if (i == 0) {
+        classConstraint = 'collection-card-begin';
+      } else if (i == 1) {
+        classConstraint = 'collection-card-middle';
+      }
+      subcategoryUpper = CollectionList.subcategory.toUpperCase();
+      if (CollectionList.special_price == '') {
+        return (
+          <Col
+            className="p-3 home-col"
+            key={i}
+            xl={6}
+            lg={6}
+            md={6}
+            sm={12}
+            xs={12}
+          >
+            <Card
+              className={
+                'image-box home-card collection-card ' + classConstraint
+              }
+            >
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                {/* <p className="card-tag">NEW</p> */}
+              </div>
+              <img
+                className="img-center"
+                src={CollectionList.image}
+                alt={CollectionList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{CollectionList.name}</p>
+                <p className="product-price-on-card">
+                  Price : {CollectionList.price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      } else {
+        return (
+          <Col
+            className="p-3 home-col"
+            key={i}
+            xl={6}
+            lg={6}
+            md={6}
+            sm={12}
+            xs={12}
+          >
+            <Card
+              className={
+                'image-box home-card collection-card ' + classConstraint
+              }
+            >
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                {/* <p className="card-tag">NEW</p> */}
+              </div>
+              <img
+                className="img-center"
+                src={CollectionList.image}
+                alt={CollectionList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{CollectionList.name}</p>
+                <p className="product-price-on-card">
+                  Price :{' '}
+                  <strike className="text-secondary">
+                    {CollectionList.price}
+                  </strike>{' '}
+                  {CollectionList.special_price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+    });
     return (
       <Fragment>
         <Container className="home-section">
@@ -22,7 +132,8 @@ export class Collection extends Component {
                 >
                   <h1 className="pt-5 pb-5 text-white">Brand Shoes</h1>
                 </Col>
-                <Col
+                {RenderView}
+                {/* <Col
                   className="p-3 home-col"
                   key={1}
                   xl={6}
@@ -79,7 +190,7 @@ export class Collection extends Component {
                       </p>
                     </Card.Body>
                   </Card>
-                </Col>
+                </Col> */}
               </Row>
             </Col>
             <Col
