e556146e35674a4a68dfea56c499fd87a12675a8
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-17 18:25:29 +0300

    consume product list API - part 3 new

--- src/components/home/NewArrival.js
+++ src/components/home/NewArrival.js
@@ -5,12 +5,27 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import shoe_4 from '../../assets/images/shoe_4.png';
 import shoe_3 from '../../assets/images/shoe_3.png';
-
+import { Link } from 'react-router-dom';
+import AppURL from '../../api/AppUrl';
+import axios from 'axios';
 export class NewArrival extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productData: [],
+    };
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+  }
+  componentDidMount() {
+    axios
+      .get(AppURL.ProductListByRemark('new'))
+      .then((response) => {
+        this.setState({ productData: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   next() {
     this.slider.slickNext();
@@ -19,6 +34,68 @@ export class NewArrival extends Component {
     this.slider.slickPrev();
   }
   render() {
+    const NewList = this.state.productData;
+    let classConstraint;
+    let subcategoryUpper;
+    const RenderView = NewList.map((NewList, i) => {
+      if (i % 2 != 0) {
+        classConstraint = 'new-arrivals-card';
+      } else if (i % 2 == 0) {
+        classConstraint = 'new-arrivals-card-even';
+      }
+      subcategoryUpper = NewList.subcategory.toUpperCase();
+      if (NewList.special_price == '') {
+        return (
+          <div className="p-3" key={i}>
+            {' '}
+            <Card className={'image-box home-card ' + classConstraint}>
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                <p className="card-tag">NEW</p>
+              </div>
+
+              <img
+                className="img-center"
+                src={NewList.image}
+                alt={NewList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{NewList.name}</p>
+                <p className="product-price-on-card">
+                  Preț : {NewList.price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+      } else {
+        return (
+          <div className="p-3" key={i}>
+            {' '}
+            <Card className={'image-box home-card ' + classConstraint}>
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                <p className="card-tag">NEW</p>
+              </div>
+
+              <img
+                className="img-center"
+                src={NewList.image}
+                alt={NewList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{NewList.name}</p>
+                <p className="product-price-on-card">
+                  Preț :{' '}
+                  <strike className="text-secondary">{NewList.price}</strike>{' '}
+                  {NewList.special_price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+      }
+    });
     var settings = {
       dots: false,
       infinite: true,
@@ -70,174 +147,7 @@ export class NewArrival extends Component {
               {...settings}
               className="new-arival-products-slider"
             >
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_3}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card-even">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_4}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_3}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card-even">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_4}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_3}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card-even">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_4}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_3}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
-              <div className="p-3">
-                {' '}
-                <Card className="image-box home-card new-arrivals-card-even">
-                  <div className="top-card">
-                    <p className="card-shoe-category">DAY-TO-DAY</p>
-                    <p className="card-tag">NEW</p>
-                  </div>
-
-                  <img
-                    className="img-center"
-                    src={shoe_4}
-                    alt="SNEAKERSI"
-                  ></img>
-                  <Card.Body>
-                    <p className="product-name-on-card">
-                      Teniși LEVI'S - Brilliant White
-                    </p>
-                    <p className="product-price-on-card">Price : 179,00 lei</p>
-                  </Card.Body>
-                </Card>
-              </div>
+              {RenderView}
             </Slider>
           </Row>
           <Row className="home-section-row">
