46c4503de25c232fc7ce24ca98d8ff79f35d5d07
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-17 19:29:46 +0300

    consume product list API - CategoryPage part 2

--- src/components/productDetails/Category.js
+++ src/components/productDetails/Category.js
@@ -1,9 +1,127 @@
 import React, { Component } from 'react';
 import { Fragment } from 'react';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 
 class Category extends Component {
   render() {
-    return <Fragment>Category</Fragment>;
+    const ProductList = this.props.ProductData;
+    const Category = this.props.Category;
+    let classConstraint;
+    let subcategoryUpper;
+    let isNew;
+    let classConstraintForNew;
+    const renderView = ProductList.map((ProductList, i) => {
+      if (i % 2 != 0) {
+        classConstraint = 'new-arrivals-card';
+      } else if (i % 2 == 0) {
+        classConstraint = 'new-arrivals-card-even';
+      }
+      if (ProductList.remark == 'new') {
+        isNew = 'NEW';
+        classConstraintForNew = 'card-tag';
+      } else {
+        isNew = '';
+        classConstraintForNew = '';
+      }
+      subcategoryUpper = ProductList.subcategory.toUpperCase();
+      if (ProductList.special_price == '') {
+        return (
+          <Col className="p-3" key={i} xl={3} lg={4} md={6} sm={6} xs={12}>
+            {' '}
+            <Card
+              className={
+                'image-box home-card category-page-product-card ' +
+                classConstraint
+              }
+            >
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                <p className={classConstraintForNew}>{isNew}</p>
+              </div>
+
+              <img
+                className="img-center"
+                src={ProductList.image}
+                alt={ProductList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{ProductList.name}</p>
+                <p className="product-price-on-card">
+                  Preț : {ProductList.price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      } else {
+        return (
+          <Col className="p-3" key={i} xl={3} lg={4} md={6} sm={6} xs={12}>
+            <Card
+              className={
+                'image-box home-card category-page-product-card ' +
+                classConstraint
+              }
+            >
+              <div className="top-card">
+                <p className="card-shoe-category">{subcategoryUpper}</p>
+                <p className={classConstraintForNew}>{isNew}</p>
+              </div>
+
+              <img
+                className="img-center"
+                src={ProductList.image}
+                alt={ProductList.subcategory}
+              ></img>
+              <Card.Body>
+                <p className="product-name-on-card">{ProductList.name}</p>
+                <p className="product-price-on-card">
+                  Preț :{' '}
+                  <strike className="text-secondary">
+                    {ProductList.price}
+                  </strike>{' '}
+                  {ProductList.special_price} lei
+                </p>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      }
+    });
+    return (
+      <Fragment>
+        <section className="category-page-section">
+          <div className="category-page-container">
+            <Container className="text-center" fluid={true}>
+              <div className="text-center my-5">
+                <h2> {Category} </h2>
+              </div>
+            </Container>
+            <Container fluid={true} className="p-4">
+              <Row className="category-page-row">
+                <Col
+                  className="border border-primary"
+                  lg={3}
+                  md={4}
+                  sm={12}
+                  xs={12}
+                >
+                  <Row className="category-page-row">Filtering fileds</Row>
+                </Col>
+                <Col
+                  className="border border-danger"
+                  lg={9}
+                  md={8}
+                  sm={12}
+                  xs={12}
+                >
+                  <Row className="category-page-row">{renderView}</Row>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </section>
+      </Fragment>
+    );
   }
 }
 
